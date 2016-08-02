dc354529189e292079257c779a124343a5c48463
Daniel Prado <daniel.prado@nlocal.com>
2016-08-02 14:24:50 +0200

    Add becomes method to api_base class

--- lib/nlocal/directory/resources/api_base.rb
+++ lib/nlocal/directory/resources/api_base.rb
@@ -3,6 +3,14 @@ module Nlocal
     class ApiBase
       include ::Her::Model
       uses_api Nlocal::Directory.configuration.api
+
+      def becomes(klass)
+        klass.new.tap do |became|
+          became.instance_variable_set("@attributes", @attributes)
+          became.instance_variable_set("@attributes_cache", @attributes_cache)
+          became.instance_variable_set("@new_record", new_record?)
+        end
+      end
     end
   end
 end
