3865c22e39b44b7647f535ff53195fb112c5bbdb
Daniel Prado <daniel.prado@nlocal.com>
2016-08-05 14:07:46 +0200

    Preload test user before token tests

--- spec/nlocal/directory/resources/token_spec.rb
+++ spec/nlocal/directory/resources/token_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
-describe Nlocal::Directory::Token do
+RSpec.describe Nlocal::Directory::Token do
+  before :all do
+    test_user
+    test_password
+  end
+
   describe :create do
     context 'ok' do
       use_vcr_cassette "token_create_ok"
-      subject {Nlocal::Directory::Token.create}
-
+      subject{ Nlocal::Directory::Token.create }
       it { expect(subject.access_token).not_to be_empty }
     end
   end
