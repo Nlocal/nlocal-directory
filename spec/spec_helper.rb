fff3d8177e9976e335fcee4661b7bf7ac1e86a2d
Daniel Prado <daniel.prado@nlocal.com>
2016-08-09 14:08:47 +0200

    Erase unnecesay stubs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,47 +37,6 @@ RSpec.configure do |config|
   config.before(:suite) do
     FactoryGirl.find_definitions
   end
-
-  config.include(Module.new do
-   def stub_api_for(klass)
-     klass.use_api (api = Her::API.new)
-
-     # Here, you would customize this for your own API (URL, middleware, etc)
-     # like you have done in your application’s initializer
-     api.setup url: "http://api.example.com" do |c|
-       # Request
-       c.use Nlocal::Directory::Middleware::Authentication
-       c.use Faraday::Request::UrlEncoded
-
-       # Response
-       c.use Her::Middleware::DefaultParseJSON
-
-       # Adapter
-       c.use Faraday::Adapter::NetHttp::FirstLevelParseJSON
-       c.adapter(:test) { |s| yield(s) }
-     end
-   end
-
-   def stub_oauth_for(klass)
-     klass.use_api (api = Her::API.new)
-
-     # Here, you would customize this for your own API (URL, middleware, etc)
-     # like you have done in your application’s initializer
-     api.setup url: "http://api.example.com" do |c|
-       # Request
-       c.use Nlocal::Directory::Middleware::Oauth
-       c.use Faraday::Request::UrlEncoded
-
-       # Response
-       c.use Her::Middleware::DefaultParseJSON
-
-       # Adapter
-       c.use Faraday::Adapter::NetHttp
-       c.adapter(:test) { |s| yield(s) }
-     end
-   end
-
-  end)
 end
 
 
