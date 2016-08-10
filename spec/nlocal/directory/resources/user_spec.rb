4375aebf7704d5f6bdfb52a909d23bf640a7a5fe
Daniel Prado <daniel.prado@nlocal.com>
2016-08-10 10:33:59 +0200

    New user tests with agencies and advertisers

--- spec/nlocal/directory/resources/user_spec.rb
+++ spec/nlocal/directory/resources/user_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
 RSpec.describe Nlocal::Directory::User do
+
   before :all do
     test_user
     test_password
+  end
+
+  before :each do
     VCR.use_cassette('token_create_ok') do
-      RequestStore.store[:token]||= Nlocal::Directory::Token.create
+      RequestStore.store[:token] = Nlocal::Directory::Token.create
     end
   end
 
@@ -12,77 +16,179 @@ RSpec.describe Nlocal::Directory::User do
     context 'get all' do
       use_vcr_cassette "users_list"
 
-      subject {
+      subject(:users) {
         Nlocal::Directory::User.all }
 
-      it { expect(subject.size).to be > 1 }
+      it { expect(users.size).to be > 1 }
     end
   end
 
   describe :create do
     context "create agency" do
-      use_vcr_cassette "agency_create"
-      subject{ RequestStore.store[:data]= Nlocal::Directory::User.create(email:"agency@test.com",
+      use_vcr_cassette "user_create"
+      subject(:agency){ RequestStore.store[:agency]= Nlocal::Directory::User.create(email:"agency@test.com",
                                               password: "agency_test",
                                               username: "agency_test",
                                               first_name: "agency",
                                               last_name: "test",
                                               type: "Agency")
-             }
+                       }
 
-      it {expect(subject).not_to be_empty}
-      it {expect(subject.id).not_to be_nil}
-      it {expect(subject.email).to eq("agency@test.com")}
-      it {expect(subject.password).to eq("agency_test")}
-      it {expect(subject.username).to eq("agency_test")}
-      it {expect(subject.first_name).to eq("agency")}
-      it {expect(subject.last_name).to eq("test")}
+      it {expect(agency).not_to be_empty}
+      it {expect(agency.id).not_to be_nil}
+      it {expect(agency.email).to eq("agency@test.com")}
+      it {expect(agency.password).to eq("agency_test")}
+      it {expect(agency.username).to eq("agency_test")}
+      it {expect(agency.first_name).to eq("agency")}
+      it {expect(agency.last_name).to eq("test")}
     end
+
+    context "create advertiser as agency" do
+      let(:agency){
+        RequestStore.store[:agency]
+      }
+      let(:token){
+        VCR.use_cassette('token_create_ok') do
+          Nlocal::Directory::Token.create(email: agency.email, password: agency.password)
+        end
+      }
+      use_vcr_cassette "user_create"
+      subject(:advertiser){
+                             RequestStore.store[:token]= token
+                             RequestStore.store[:advertiser]= Nlocal::Directory::User.create(email:"advertiser@test.com",
+                                                                                             password: "advertiser_test",
+                                                                                             username: "advertiser_test",
+                                                                                             first_name: "advertiser",
+                                                                                             last_name: "test",
+                                                                                             type: "Advertiser")
+                           }
+      it {expect(advertiser).not_to be_empty}
+      it {expect(advertiser.id).not_to be_nil}
+      it {expect(advertiser.email).to eq("advertiser@test.com")}
+      it {expect(advertiser.password).to eq("advertiser_test")}
+      it {expect(advertiser.username).to eq("advertiser_test")}
+      it {expect(advertiser.first_name).to eq("advertiser")}
+      it {expect(advertiser.last_name).to eq("test")}
+    end
+
   end
 
   describe :show do
-    context "get user" do
+    context "get agency" do
       use_vcr_cassette "user_show"
-      subject{
-        Nlocal::Directory::User.find(RequestStore.store[:data].id) }
+      subject(:agency){
+        Nlocal::Directory::User.find(RequestStore.store[:agency].id) }
 
-      it {expect(subject).not_to be_empty}
-      it {expect(subject.id).not_to be_nil}
+      it {expect(agency).not_to be_empty}
+      it {expect(agency.id).not_to be_nil}
+      it {expect(agency.email).to eq("agency@test.com")}
+      it {expect(agency.username).to eq("agency_test")}
+      it {expect(agency.first_name).to eq("agency")}
+      it {expect(agency.last_name).to eq("test")}
+    end
+
+    context "get advertiser as agency" do
+      let(:agency){
+        RequestStore.store[:agency]
+      }
+      let(:token){
+        VCR.use_cassette('token_create_ok') do
+          Nlocal::Directory::Token.create(email: agency.email, password: agency.password)
+        end
+      }
+      use_vcr_cassette "user_show"
+      subject(:advertiser){
+                            RequestStore.store[:token]= token
+                            Nlocal::Directory::User.find(RequestStore.store[:advertiser].id)
+                          }
+
+      it {expect(advertiser).not_to be_empty}
+      it {expect(advertiser.id).not_to be_nil}
+      it {expect(advertiser.email).to eq("advertiser@test.com")}
+      it {expect(advertiser.username).to eq("advertiser_test")}
+      it {expect(advertiser.first_name).to eq("advertiser")}
+      it {expect(advertiser.last_name).to eq("test")}
     end
   end
 
   describe :update do
-    before :context do
-      VCR.use_cassette('user_show') do
-        @user= Nlocal::Directory::User.find(RequestStore.store[:data].id)
-      end
-    end
-    context "update user" do
+
+    context "update agency" do
+      let(:agency) {
+        RequestStore.store[:agency]
+      }
       use_vcr_cassette "user_update"
-      subject{ @user.assign_attributes(first_name: "test",
+      subject(:updated_agency){ agency.assign_attributes(first_name: "test",
                                        last_name: "agency")
-               @user.save
-             }
-      it {expect(subject).not_to be_empty}
-      it {expect(subject.id).not_to be_nil}
-      it {expect(subject.first_name).to eq("test")}
-      it {expect(subject.last_name).to eq("agency")}
+                                agency.save
+                              }
+      it {expect(updated_agency).not_to be_empty}
+      it {expect(updated_agency.id).not_to be_nil}
+      it {expect(updated_agency.first_name).to eq("test")}
+      it {expect(updated_agency.last_name).to eq("agency")}
+    end
+
+    context "update advertiser as agency" do
+      let(:agency){
+        RequestStore.store[:agency]
+      }
+      let(:token){
+        VCR.use_cassette('token_create_ok') do
+          Nlocal::Directory::Token.create(email: agency.email, password: agency.password)
+        end
+      }
+      let(:advertiser){
+        RequestStore.store[:advertiser]
+      }
+      use_vcr_cassette "user_update"
+      subject(:updated_advertiser){
+                                RequestStore.store[:token]= token
+                                advertiser.assign_attributes(first_name: "test",
+                                                             last_name: "advertiser")
+                                advertiser.save
+                              }
+      it {expect(updated_advertiser).not_to be_empty}
+      it {expect(updated_advertiser.id).not_to be_nil}
+      it {expect(updated_advertiser.first_name).to eq("test")}
+      it {expect(updated_advertiser.last_name).to eq("advertiser")}
     end
   end
 
   describe :delete do
-    before :context do
-      VCR.use_cassette('user_show') do
-        @user= Nlocal::Directory::User.find(RequestStore.store[:data].id)
-      end
-    end
-    context "delete_user" do
-      use_vcr_cassette "user_delete"
-      subject{ @user.destroy }
 
-      it {expect(subject).not_to be_empty}
-      it {expect(subject.id).not_to be_nil}
+    context "advertiser delete as agency" do
+      let(:agency){
+        RequestStore.store[:agency]
+      }
+      let(:token){
+        VCR.use_cassette('token_create_ok') do
+          Nlocal::Directory::Token.create(email: agency.email, password: agency.password)
+        end
+      }
+      let(:advertiser){
+        RequestStore.store[:advertiser]
+      }
+      use_vcr_cassette "user_delete"
+      subject(:destroyed_advertiser){
+                                      RequestStore.store[:token]= token
+                                      advertiser.destroy
+                                     }
+      it {expect(destroyed_advertiser).not_to be_empty}
+      it {expect(destroyed_advertiser.id).not_to be_nil}
     end
+
+    context "agency delete" do
+      let(:agency){
+        RequestStore.store[:agency]
+      }
+      use_vcr_cassette "user_delete"
+      subject(:destroyed_agency){ agency.destroy }
+
+      it {expect(destroyed_agency).not_to be_empty}
+      it {expect(destroyed_agency.id).not_to be_nil}
+    end
+
+
   end
 
 
